6fcbcfe27328313597def2b0347825a4a03f8f85
Rashedul Alam <rashedulalam112112@gmail.com>
2021-02-25 20:39:57 +0600

    Configured mikro orm

---
+++ src/config/app.config.ts
@@ -0,0 +1,2 @@
+export const __prod__: boolean = process.env.NODE_ENV === 'production' || false
+export const appDebug: boolean = process.env.APP_DEBUG === 'true' || !__prod__ || true
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,18 @@
-console.log('hello')
+import { MikroORM } from '@mikro-orm/core'
+
+import mikroConfig from './config/mikro-orm.config'
+import { Post } from './entities/Post'
+
+const main = async () => {
+  const orm = await MikroORM.init(mikroConfig)
+  await orm.getMigrator().up()
+  // const post = orm.em.create(Post, { title: 'My first post' })
+  // await orm.em.persistAndFlush(post)
+
+  // const posts = await orm.em.find(Post, {})
+  // console.log(posts)
+}
+
+main().catch((err) => {
+  console.error(err)
+})
