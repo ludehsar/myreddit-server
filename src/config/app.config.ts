4efa005accbf4bb7508a4bf095c15e303259fe39
Rashedul Alam <rashedulalam112112@gmail.com>
2021-02-25 22:17:37 +0600

    Implemented basic post crud in graphql

--- src/config/app.config.ts
+++ src/config/app.config.ts
@@ -1,2 +1,3 @@
 export const __prod__: boolean = process.env.NODE_ENV === 'production' || false
 export const appDebug: boolean = process.env.APP_DEBUG === 'true' || !__prod__ || true
+export const port = process.env.PORT || 4000
--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,33 @@
+import 'reflect-metadata'
 import { MikroORM } from '@mikro-orm/core'
+import express from 'express'
+import { ApolloServer } from 'apollo-server-express'
+import { buildSchema } from 'type-graphql'
 
 import mikroConfig from './config/mikro-orm.config'
-import { Post } from './entities/Post'
+import { port } from './config/app.config'
+import { HelloResolver } from './resolvers/hello'
+import { PostResolver } from './resolvers/post'
 
 const main = async () => {
   const orm = await MikroORM.init(mikroConfig)
   await orm.getMigrator().up()
-  // const post = orm.em.create(Post, { title: 'My first post' })
-  // await orm.em.persistAndFlush(post)
 
-  // const posts = await orm.em.find(Post, {})
-  // console.log(posts)
+  const app = express()
+
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [HelloResolver, PostResolver],
+      validate: false
+    }),
+    context: () => ({ em: orm.em })
+  })
+
+  apolloServer.applyMiddleware({ app })
+
+  app.listen(port, () => {
+    console.log(`Server is listening on port ${port}.`)
+  })
 }
 
 main().catch((err) => {
